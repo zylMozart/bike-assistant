b2f73535f0bd193dea866acf6caaf7a60042adf9
idevlab <idevlab@outlook.com>
2020-12-22 11:26:47 +0800

    ui improve

--- view/user-interface/src/store/index.js
+++ view/user-interface/src/store/index.js
@@ -16,13 +16,25 @@ export default new Vuex.Store({
           lat:30,
           lng:122,
         },
-        temperature:23.9
+        temperature:23.9,
+        speed:100,
+        altitude:100,
+        pressure:8
     }
     
   },
   mutations: {
     setTemperature(state, payload){
       state.environment.temperature=payload;
+    },
+    setPressure(state, payload){
+      state.environment.pressure=payload;
+    },
+    setSpeed(state, payload){
+      state.environment.speed=payload;
+    },
+    setAltitude(state, payload){
+      state.environment.altitude=payload;
     },
     setLight1(state, payload){
       state.device.light.light1=payload;
@@ -42,10 +54,16 @@ export default new Vuex.Store({
       let res = await (await fetch("http://127.0.0.1:8001/temperature")).json();
       context.commit('setTemperature',res.data);
     },
-    async getGeo(context) {
+    async updatePressure(context) {
+      let res = await (await fetch("http://127.0.0.1:8001/pressure")).json();
+      context.commit('setPressure',res.data);
+    },
+    async updateGeo(context) {
       let res = await (await fetch("http://127.0.0.1:8001/location")).json();
-      context.commit('setGeo',res.data);
-      return res.data;
+      context.commit('setSpeed',res.data.speed);
+      context.commit('setAltitude',res.data.altitude);
+      context.commit('setGeo',{lat:res.data.latitude,lng:res.data.longitude});
+      return {lat:res.data.latitude,lng:res.data.longitude}
     },
     async updateLight1(context,payload) {
       let res = await (await fetch("http://127.0.0.1:8001/set/light/"+payload)).json();
